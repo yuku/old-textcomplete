caa199c3e1315daac8ec014db7c47bf24537bcd4
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-01-30 03:27:59 +0000

    Enable to build javascript with babel and browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
-var gulp = require('gulp');
+var gulp        = require('gulp');
+
+var babelify    = require('babelify');
+var browserify  = require('browserify');
+var source      = require('vinyl-source-stream');
  
 gulp.task('build', function () {
- 
+    return browserify({entries: './src/index.js', debug: true})
+        .transform('babelify', {presets: ['es2015']})
+        .bundle()
+        .pipe(source('index.js'))
+        .pipe(gulp.dest('./dist'));
 });
  
 gulp.task('watch', ['build'], function () {
-    gulp.watch('./src/js/*.js', ['build']);
+    gulp.watch('./src/*.js', ['build']);
 });
  
 gulp.task('default', ['build', 'watch']);
