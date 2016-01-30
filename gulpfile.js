a5e964e2d71554fedac79d3ceff28c20fbbd1e7c
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-01-30 03:46:19 +0000

    Add editorconfig and tweak js indent size

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,19 @@ var sourcemaps  = require('gulp-sourcemaps');
 var uglify      = require('gulp-uglify');
  
 gulp.task('build', function () {
-    return browserify({entries: './src/index.js', debug: true})
-        .transform('babelify', {presets: ['es2015']})
-        .bundle()
-        .pipe(source('index.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./dist'));
+  return browserify({entries: './src/index.js', debug: true})
+    .transform('babelify', {presets: ['es2015']})
+    .bundle()
+    .pipe(source('index.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./dist'));
 });
  
 gulp.task('watch', ['build'], function () {
-    gulp.watch('./src/*.js', ['build']);
+  gulp.watch('./src/*.js', ['build']);
 });
  
 gulp.task('default', ['build', 'watch']);
