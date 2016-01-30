9c36e3a3f228e00ac3100dd944bc0d91afd61630
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-01-30 03:32:55 +0000

    Uglify dist script

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,17 @@ var gulp        = require('gulp');
 
 var babelify    = require('babelify');
 var browserify  = require('browserify');
+var buffer      = require('vinyl-buffer');
 var source      = require('vinyl-source-stream');
+var uglify      = require('gulp-uglify');
  
 gulp.task('build', function () {
     return browserify({entries: './src/index.js', debug: true})
         .transform('babelify', {presets: ['es2015']})
         .bundle()
         .pipe(source('index.js'))
+        .pipe(buffer())
+        .pipe(uglify())
         .pipe(gulp.dest('./dist'));
 });
  
