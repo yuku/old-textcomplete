561513d63b8a61cf1bb01196f76a37494197d218
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-01-30 15:19:11 +0000

    Add Strategy#test()

--- src/strategy.js
+++ src/strategy.js
@@ -1,8 +1,47 @@
+import {isFunction} from 'lodash';
+
+/**
+ * Encapsulate a single strategy.
+ */
 export default class Strategy {
   /**
    * @param {Object} strategy
    */
   constructor(strategy) {
     this.strategy = strategy;
+  }
+
+  /**
+   * @param {string} text - Head to input cursor.
+   * @returns {Array|false}
+   */
+  test(text) {
+    var match = text.match(this.getMatchRegexp(text));
+    return match ? { strategy: this, query: match[this.index], match: match } : false;
+  }
+
+  /**
+   * @private
+   * @param {string} text
+   * @returns {RegExp}
+   */
+  getMatchRegexp(text) {
+    return isFunction(this.match) ? this.match(text) : this.match;
+  }
+
+  /**
+   * @private
+   * @returns {RegExp|Function}
+   */
+  get match() {
+    return this.strategy.match;
+  }
+
+  /**
+   * @private
+   * @returns {Number}
+   */
+  get index() {
+    return this.strategy.index || 2;
   }
 }
--- test/strategy.js
+++ test/strategy.js
@@ -1,13 +1,65 @@
 import Strategy from '../src/strategy';
 
+import {isArray} from 'lodash';
+
 var assert = require('power-assert');
 
-describe('Strategy', () => {
-  describe('#strategy', () => {
-    it('should be an object', () => {
+describe('Strategy', function () {
+  describe('#strategy', function () {
+    it('should be an object', function () {
       var object = {};
       var strategy = new Strategy(object);
-      assert(strategy.strategy === object);
+      assert.strictEqual(strategy.strategy, object);
+    });
+  });
+
+  describe('#test()', function () {
+    context('when match param is a function' , function () {
+      beforeEach(function () {
+        var object = {
+          match: function () { return /(^|\s)@(\w+)$/; },
+        };
+        this.strategy = new Strategy(object);
+      });
+
+      context("and given text matches to the function's result", function () {
+        it('should return object with strategy, query and match', function () {
+          var result = this.strategy.test('@hello');
+          assert.strictEqual(result.strategy, this.strategy);
+          assert.strictEqual(result.query, 'hello');
+          assert.ok(isArray(result.match));
+        });
+      });
+
+      context("and given text does not match to the function's result", function () {
+        it('should return false', function () {
+          assert.equal(this.strategy.test('hello'), false);
+        });
+      });
+    });
+
+    context('when match param is a regexp', function () {
+      beforeEach(function () {
+        var object = {
+          match: /(^|\s)@(\w+)$/,
+        };
+        this.strategy = new Strategy(object);
+      });
+
+      context("and given text matches to the function's result", function () {
+        it('should return object with strategy, query and match', function () {
+          var result = this.strategy.test('@hello');
+          assert.strictEqual(result.strategy, this.strategy);
+          assert.strictEqual(result.query, 'hello');
+          assert.ok(isArray(result.match));
+        });
+      });
+
+      context("and given text does not match to the function's result", function () {
+        it('should return false', function () {
+          assert.equal(this.strategy.test('hello'), false);
+        });
+      });
     });
   });
 });
