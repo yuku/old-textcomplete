10d1ab7ed4468dad9948b79bd96499a4350e2b8c
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-02-04 00:15:03 +0000

    Add Completer#extractQuery private method

--- src/completer.js
+++ src/completer.js
@@ -13,4 +13,18 @@ export default class Completer {
     this.strategies.push(strategy);
     return this;
   }
+
+  /**
+   * @private
+   * @param {string} text - Head to input cursor.
+   * @returns {?Query}
+   */
+  extractQuery(text) {
+    var i;
+    for (i = 0; i < this.strategies.length; i++) {
+      let query = this.strategies[i].buildQuery(text);
+      if (query) { return query; }
+    }
+    return null;
+  }
 }
