14b7d69eb172a34c0816c7e63719f5f7816cdcfa
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-02-04 00:54:49 +0000

    Remove meaning less test case

--- test/textcomplete.js
+++ test/textcomplete.js
@@ -1,18 +1,10 @@
 import Textcomplete from '../src/textcomplete';
-import Completer from '../src/completer';
 
 const assert = require('power-assert');
 
 import {getTextarea} from './test-helper';
 
 describe('Textcomplete', function () {
-  describe('#completer', function () {
-    it('should be a Completer', function () {
-      var textcomplete = new Textcomplete(getTextarea());
-      assert.ok(textcomplete.completer instanceof Completer);
-    });
-  });
-
   describe('#register', function () {
     it('should return itself', function () {
       var textcomplete = new Textcomplete(getTextarea());
