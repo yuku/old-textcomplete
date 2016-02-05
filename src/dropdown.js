a9ffcbbab7989fa62246618033c99756082ff0e3
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-02-05 11:21:21 +0000

    Add Dropdown.createElement and Dropdown#el

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,10 +1,36 @@
+import {extend, uniqueId} from 'lodash';
+
 /**
  * Encapsulate a dropdown view.
  */
 export default class Dropdown {
+  /**
+   * @returns {HTMLUListElement}
+   */
+  static createElement() {
+    var el = document.createElement('ul');
+    el.className = 'dropdown-menu textcomplete-dropdown';
+    el.id = uniqueId('textcomplete-dropdown-');
+    extend(el.style, {
+      display: 'none',
+      left: 0,
+      position: 'absolute',
+      zIndex: 10000,
+    });
+    document.body.appendChild(el);
+    return el;
+  }
+
   constructor() {
-    this.el = null;
     this.shown = false;
+  }
+
+  /**
+   * @returns {HTMLUListElement}
+   */
+  get el() {
+    this._el || (this._el = Dropdown.createElement());
+    return this._el;
   }
 
   /**
---
+++ test/dropdown.js
@@ -0,0 +1,35 @@
+import Dropdown from '../src/dropdown';
+
+const assert = require('power-assert');
+const jsdom = require('jsdom');
+const sinon = require('sinon');
+
+describe('Dropdown', function () {
+  describe('.createElement', function () {
+    before(function () {
+      global.document = jsdom.jsdom();
+    });
+
+    after(function () {
+      delete global.document;
+    });
+
+    it('should return a HTMLUListElement under body element', function () {
+      var el = Dropdown.createElement();
+      assert(el instanceof document.defaultView.HTMLUListElement);
+      assert.equal(el.parentNode, document.body);
+    });
+  });
+
+  describe('#el', function () {
+    it('should return a object created by Dropdown.createElement()', function () {
+      var result = {};
+      var stub = sinon.stub(Dropdown, 'createElement', () => { return result; });
+      var dropdown = new Dropdown();
+      assert.strictEqual(dropdown.el, result);
+      assert(stub.calledOnce);
+      assert.strictEqual(dropdown.el, result);
+      assert(stub.calledOnce); // Dropdown.createElement is not called.
+    });
+  });
+});
