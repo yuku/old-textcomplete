cf62e61fa9f85d682626b774fb74eb2ee33cd0c8
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-02-05 14:51:49 +0000

    Add Dropdown#show, #hide and #clear

--- src/dropdown.js
+++ src/dropdown.js
@@ -23,9 +23,11 @@ export default class Dropdown {
 
   constructor() {
     this.shown = false;
+    this.items = [];
   }
 
   /**
+   * @private
    * @returns {HTMLUListElement}
    */
   get el() {
@@ -38,13 +40,58 @@ export default class Dropdown {
    *
    * @param {SearchResult[]} _searchResults
    * @param {{top: number, left: number}} _cursorPosition
+   * @returns {this}
    */
   render(_searchResults, _cursorPosition) {
+    this.show();
+    return this;
   }
 
   /**
    * Hide the dropdown then sweep out items.
+   *
+   * @returns {this}
    */
   deactivate() {
+    return this.hide().clear();
+  }
+
+  /**
+   * Show the element.
+   *
+   * @private
+   * @returns {this}
+   */
+  show() {
+    if (!this.shown) {
+      this.el.style.display = 'block';
+      this.shown = true;
+    }
+    return this;
+  }
+
+  /**
+   * Hide the element.
+   *
+   * @private
+   * @returns {this}
+   */
+  hide() {
+    if (this.shown) {
+      this.el.style.display = 'none';
+      this.shown = false;
+    }
+    return this;
+  }
+
+  /**
+   * Clear search results.
+   *
+   * @private
+   * @returns {this}
+   */
+  clear() {
+    this.items = [];
+    return this;
   }
 }
--- test/dropdown.js
+++ test/dropdown.js
@@ -4,7 +4,7 @@ const assert = require('power-assert');
 const jsdom = require('jsdom');
 
 describe('Dropdown', function () {
-  describe('.createElement', function () {
+  function loadDocument() {
     before(function () {
       global.document = jsdom.jsdom();
     });
@@ -12,6 +12,10 @@ describe('Dropdown', function () {
     after(function () {
       delete global.document;
     });
+  }
+
+  describe('.createElement', function () {
+    loadDocument.call(this);
 
     it('should return a HTMLUListElement under body element', function () {
       var el = Dropdown.createElement();
@@ -29,6 +33,50 @@ describe('Dropdown', function () {
       assert(stub.calledOnce);
       assert.strictEqual(dropdown.el, result);
       assert(stub.calledOnce); // Dropdown.createElement is not called.
+    });
+  });
+
+  describe('#render', function () {
+    loadDocument.call(this);
+
+    it('should return itself', function () {
+      var dropdown = new Dropdown();
+      assert.strictEqual(dropdown.render(), dropdown);
+    });
+
+    context('when it is hidden', function () {
+      it('should change #shown from false to true', function () {
+        var dropdown = new Dropdown();
+        dropdown.shown = false;
+        dropdown.render();
+        assert(dropdown.shown);
+      });
+    });
+  });
+
+  describe('#deactivate', function () {
+    loadDocument.call(this);
+
+    it('should return itself', function () {
+      var dropdown = new Dropdown();
+      assert.strictEqual(dropdown.deactivate(), dropdown);
+    });
+
+    it('should empty #items', function () {
+      var dropdown = new Dropdown();
+      dropdown.items = [1];
+      assert.equal(dropdown.items.length, 1);
+      dropdown.deactivate();
+      assert.equal(dropdown.items.length, 0);
+    });
+
+    context('when it is shown', function () {
+      it('should change #shown from true to false', function () {
+        var dropdown = new Dropdown();
+        dropdown.shown = true;
+        dropdown.deactivate();
+        assert(!dropdown.shown);
+      });
     });
   });
 });
