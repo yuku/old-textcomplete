78f4fa756d2c8dfcaf8164a8de92b66d5db419c3
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-02-07 18:35:40 +0900

    Refactor gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,56 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
+var path = require('path');
 
 require('babel-register');
 
-var browserify  = require('browserify');
-var buffer      = require('vinyl-buffer');
-var eslint      = require('gulp-eslint');
-var espower     = require('gulp-espower');
-var isparta     = require('isparta');
-var istanbul    = require('gulp-istanbul');
-var mocha       = require('gulp-mocha');
-var rename      = require('gulp-rename');
-var source      = require('vinyl-source-stream');
-var sourcemaps  = require('gulp-sourcemaps');
-var uglify      = require('gulp-uglify');
+var paths = {
+  dist: './dist/',
+  src: {
+    js: './src/*.js',
+    entry: 'index.js',
+  },
+  test: {
+    js: './test/*.js',
+    dest: './powered-test/',
+    runner: 'test-runner.js',
+  },
+};
 
 gulp.task('lint', function () {
-  return gulp.src(['src/*.js', 'test/*.js'])
+  var eslint = require('gulp-eslint');
+  return gulp.src([paths.src.js, paths.test.js])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
  
 gulp.task('build', ['lint'], function () {
-  return browserify({entries: './src/index.js', debug: true})
+  var browserify = require('browserify');
+  var rename = require('gulp-rename');
+  var buffer = require('vinyl-buffer');
+  var uglify = require('gulp-uglify');
+  var sourcemaps = require('gulp-sourcemaps');
+  var source = require('vinyl-source-stream');
+  return browserify({ entries: path.join(path.dirname(paths.src.js), paths.src.entry) })
     .transform('babelify')
     .bundle()
-    .pipe(source('index.js'))
+    .pipe(source(paths.src.entry))
     .pipe(rename('bundle.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init())
     .pipe(uglify())
     .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest(paths.dist));
 });
- 
+
 gulp.task('watch', ['build'], function () {
-  gulp.watch('./src/*.js', ['build']);
+  gulp.watch(paths.src.js, ['build']);
 });
 
 gulp.task('istanbul', function () {
-  return gulp.src('./src/**/*.js')
+  var isparta = require('isparta');
+  var istanbul = require('gulp-istanbul');
+  return gulp.src(paths.src.js)
     .pipe(istanbul({
       instrumenter: isparta.Instrumenter,
     }))
@@ -47,15 +58,18 @@ gulp.task('istanbul', function () {
 });
 
 gulp.task('power-assert', function () {
-  return gulp.src('./test/*.js')
+  var espower = require('gulp-espower');
+  return gulp.src(paths.test.js)
     .pipe(espower())
-    .pipe(gulp.dest('./powered-test/'));
+    .pipe(gulp.dest(paths.test.dest));
 });
 
 gulp.task('test', ['power-assert', 'istanbul'], function () {
-  return gulp.src('./powered-test/test-runner.js')
+  var mocha = require('gulp-mocha');
+  var istanbul = require('gulp-istanbul');
+  return gulp.src(path.join(paths.test.dest, paths.test.runner))
     .pipe(mocha())
     .pipe(istanbul.writeReports());
 });
  
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['watch']);
